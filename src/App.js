c3e666bfceff9a98b8c217fc02c3dd54cc38a138
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-03 12:22:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
-function App() {
+import Debits from "./components/Debits";
+
+import axios from "axios";
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountBalance: 0,
+      debits: [],
+      credits: []
+    }
+  }
+
+  async componentDidMount() {
+    let debits = await axios.get("https://moj-api.herokuapp.com/debits")
+    let credits = await axios.get("https://moj-api.herokuapp.com/credits")
+   
+    //get data from API response
+    debits = debits.data
+    credits = credits.data
+
+    let debitSum = 0, creditSum = 0;
+    debits.forEach((debit) => {
+      debitSum += debit.amount
+    })
+    credits.forEach((credit) => {
+      creditSum += credit.amount
+    })
+
+    let accountBalance = creditSum - debitSum;
+    this.setState({debits, credits, accountBalance});
+  } 
+
+
+  addDebit = (e) => {
+    //send to debits view via props
+    //updates state based off user input
+    e.preventDefault()
+    let { debits } = this.state
+    let balance = this.state.accountBalance;
+
+    const description  = e.target[0].value
+    const amount  = Number(e.target[1].value)
+    const today = new Date();
+
+    //formatting to match other dates
+    const month = today.getMonth() + 1;
+    const date = today.getFullYear().toString() + "-" + month.toString() + "-" + today.getDate().toString();
+    
+    const newDebit = {description, amount, date}
+    balance = balance - amount;
+    debits = [...debits, newDebit]
+    this.setState({debits: debits, accountBalance: balance})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React Router!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/debits" element={<Debits addDebit={this.addDebit} debits={this.state.debits} />} />
+        </Routes>
+        <h3>{this.state.accountBalance}</h3>
+      </div>
+    );
+  }
+
+
+}
+
+
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Welcome to the homepage!</h2>
+      <Link to="/debits">Debits</Link>
     </div>
   );
 }
+
 
 export default App;
